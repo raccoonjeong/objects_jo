5799a59dc350f151b7722d9a538e8f54a352460e
raccoonjeong <rana3782@gmail.com>
2020-07-09 00:35:17 +0900

    Initial commit

---
+++ object_coding/src/org/obcoding/ch01/Bag.java
@@ -0,0 +1,36 @@
+package org.obcoding.ch01;
+
+public class Bag {
+    private Long amount;
+    private Invitation invitation;
+    private Ticket ticket;
+
+    public Bag(Long amount) {
+        this(null, amount);
+    }
+
+    public Bag(Invitation invitation, Long amount) {
+        this.invitation = invitation;
+        this.amount = amount;
+    }
+
+    public boolean hasInvitaion() {
+        return invitation != null;
+    }
+
+    public boolean hasTicket() {
+        return this.ticket != ticket;
+    }
+
+    public void setTicket(Ticket ticket) {
+        this.ticket = ticket;
+    }
+
+    public void minusAmount(Long amount) {
+        this.amount -= amount;
+    }
+
+    public void plusAmount(Long amount) {
+        this.amount += amount;
+    }
+}
---
+++ object_coding/src/org/obcoding/ch01/Invitation.java
@@ -0,0 +1,7 @@
+package org.obcoding.ch01;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+    private LocalDateTime when;
+}
