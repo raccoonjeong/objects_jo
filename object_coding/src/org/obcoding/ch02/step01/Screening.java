0ca683a10d910fe52222dd9d34c8c0de23d4287b
raccoonjeong <rana3782@gmail.com>
2022-07-21 19:58:02 +0900

    챕터2 초반

---
+++ object_coding/src/org/obcoding/ch02/step01/Screening.java
@@ -0,0 +1,28 @@
+package org.obcoding.ch02.step01;
+
+import java.time.LocalDateTime;
+
+// 상영: 사용자들이 예매하는 대상
+public class Screening {
+    private Movie movie;
+    private int sequence;
+    private LocalDateTime whenScreened;
+
+    public Screening(Movie movie, int sequence, LocalDateTime whenScreened) {
+        this.movie = movie;
+        this.sequence = sequence;
+        this.whenScreened = whenScreened;
+    }
+
+    public LocalDateTime getStartTime() {
+        return whenScreened;
+    }
+
+    public boolean isSequence(int sequence) {
+        return this.sequence == sequence;
+    }
+
+    public Money getMovieFee() {
+        return movie.getFee();
+    }
+}
