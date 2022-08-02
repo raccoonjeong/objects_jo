131a78b7c44ce2b3de352eb37b9b1148367cb33b
raccoonjeong <rana3782@gmail.com>
2022-08-02 23:08:58 +0900

    챕터04 끝

---
+++ object_coding/src/org/obcoding/ch02/step01/Main.java
@@ -0,0 +1,17 @@
+package org.obcoding.ch02.step01;
+
+import java.time.Duration;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // 사용 예시
+//        Movie avatar = 
+//                new Movie("아바타", 
+//                        Duration.ofMinutes(120), 
+//                        Money.wons(10000), 
+//                        new AmountDiscountPolicy(Money.wons(800), 
+//                        ...))；
+        // avatar.changeDiscountPolicy(new PercentDiscountPolicy(0.1, ...))；
+    }
+}
